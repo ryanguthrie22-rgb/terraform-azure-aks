f61b8cd6d76c6f19714077a2e232c3d2267a1468
Ryan A. Guthrie <ryana.guthrie@hse.ie>
2026-03-10 10:32:39 +0000

    Add dev Terraform variable file for GitHub Actions

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstate2234618784"
+    container_name       = "tfstate"
+    key                  = "terraform-azure-aks-dev.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
